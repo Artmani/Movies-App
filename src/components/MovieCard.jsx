496f7430f099de6d4a1e0ed3129f3fc2fef479fd
Artmaniqq <Artmani1337@mail.ru>
2025-01-27 16:52:11 +0300

    звезды и рейтинг

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { format } from 'date-fns'
 import { ru } from 'date-fns/locale'
+import { Rate } from 'antd'
 
 import { truncateText } from '../utils/truncateText'
 import '../styles/MovieCard.css'
 
 function MovieCard({ movie }) {
-  const { title, release_date: releaseDate, overview, poster_path: posterPath } = movie
+  const { title, release_date: releaseDate, overview, poster_path: posterPath, vote_average: rating } = movie
   const formattedDate = releaseDate ? format(new Date(releaseDate), 'd MMM yyyy', { locale: ru }) : 'Неизвестно'
 
   const posterUrl = posterPath ? `https://image.tmdb.org/t/p/w300/${posterPath}` : null
+
+  const getRatingColor = (value) => {
+    if (value >= 7) return '#66E900'
+    if (value >= 5) return '#E9D100'
+    if (value >= 3) return '#E97E00'
+    return '#E90000'
+  }
+
+  const [userRating, setUserRating] = useState(null)
+
+  const handleRateChange = (value) => {
+    setUserRating(value)
+    console.log(`Рейтинг фильма "${title}" установлен на ${value}`)
+  }
 
   return (
     <div className="movie-card">
@@ -23,9 +38,20 @@ function MovieCard({ movie }) {
       </div>
 
       <div className="movie-card__content">
-        <h3 className="movie-card__title">{title}</h3>
+        <div className="movie-card__zag">
+          <h3 className="movie-card__title">{title}</h3>
+          <div
+            className="movie-card__rating"
+            style={{
+              borderColor: getRatingColor(rating),
+            }}
+          >
+            {rating || 'N/A'}
+          </div>
+        </div>
         <p className="movie-card__date">{formattedDate}</p>
         <div className="movie-card__overview">{truncateText(overview, 120)}</div>
+        <Rate className="movie-card__rate" count={10} value={userRating} onChange={handleRateChange} />
       </div>
     </div>
   )
@@ -38,6 +64,7 @@ MovieCard.propTypes = {
     release_date: PropTypes.string,
     overview: PropTypes.string,
     poster_path: PropTypes.string,
+    vote_average: PropTypes.number,
   }).isRequired,
 }
 
